f71b9fab56d12495b77f3d6bd6bed74d65cc554e
Thomas Rabaix <thomas.rabaix@gmail.com>
2019-11-06 12:25:57 +0100

    feat(sitemap): create a dedicated createSitemapPageBuilder helper

--- examples/nextjs-with-remoteapi/server.js
+++ examples/nextjs-with-remoteapi/server.js
@@ -1,16 +1,10 @@
 const express = require("express");
 const rendrApi = require("@ekino/rendr-api");
-const rendrSitemap = require("@ekino/rendr-sitemap");
 
 const api = require("./services/api");
-const sitemap = require("./services/sitemap");
 
 const app = express();
 
 app.use("/api", rendrApi.createApi(api.loader));
-app.use(
-  "/sitemap.xml",
-  rendrSitemap.createSitemapRequestHandler(sitemap.generator)
-);
 
 module.exports = app;

eaac8c6bbfbd7397737111fb23a49e1bc8f52ee5
fancyweb <calvet.thomas@gmail.com>
2019-11-15 13:57:44 +0100

    feat(drupal): add template entity

---
+++ packages/cms-drupal/src/Entity/Template.php
@@ -0,0 +1,62 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\ekino_rendr\Entity;
+
+use Drupal\Core\Config\Entity\ConfigEntityBase;
+use Drupal\ekino_rendr\Model\Container;
+
+/**
+ * @ConfigEntityType(
+ *   id="ekino_rendr_template",
+ *   label=@Translation("Template"),
+ *
+ *   admin_permission="administer ekino_rendr templates",
+ *   config_export={
+ *      "id",
+ *      "label",
+ *      "containers"
+ *   },
+ *   entity_keys={
+ *      "id"="id",
+ *      "label"="label",
+ *   },
+ *   handlers={
+ *      "list_builder"="Drupal\ekino_rendr\Entity\TemplateListBuilder",
+ *      "route_provider" = {
+ *          "html"="Drupal\Core\Entity\Routing\AdminHtmlRouteProvider",
+ *      },
+ *   },
+ *   label_collection=@Translation("Templates"),
+ *   label_count=@PluralTranslation(
+ *      singular="@count template",
+ *      plural="@count templates",
+ *   ),
+ *   label_singular=@Translation("template"),
+ *   label_plural=@Translation("templates"),
+ *   links={
+ *      "canonical"="/admin/config/ekino_rendr/template/{ekino_rendr_template}",
+ *      "collection"="/admin/config/ekino_rendr/template"
+ *   }
+ * )
+ */
+final class Template extends ConfigEntityBase implements TemplateInterface
+{
+    const ID = 'ekino_rendr_template';
+
+    /**
+     * @return Container[]
+     */
+    public function getContainers(): array
+    {
+        $containers = $this->get('containers');
+        if (!\is_array($containers)) {
+            return [];
+        }
+
+        return \array_map(static function ($row): Container {
+            return new Container($row['id'], $row['label']);
+        }, $containers);
+    }
+}

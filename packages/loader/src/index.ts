326484996a8962cee328e90838fc63b7cc84b90a
Sylvain.Rascar <sylvain.rascar@ekino.com>
2019-12-03 18:34:49 +0000

    fix(errors): Update errorBoundaryLoader behaviour

--- packages/loader/src/index.ts
+++ packages/loader/src/index.ts
@@ -1,5 +1,12 @@
-import { Loader } from "./types";
-import { Page, createPage, NotFoundError } from "@ekino/rendr-core";
+import { Loader, MaybePage } from "./types";
+import {
+  Page,
+  createPage,
+  NotFoundError,
+  RendrError,
+  RequestCtx
+} from "@ekino/rendr-core";
+import { type } from "os";
 
 export * from "./types";
 export * from "./services/inmemory";
@@ -28,20 +35,63 @@ export const createChainedLoader: (loaders: Loader[]) => Loader = loaders => {
   };
 };
 
-// Simple version of a loader that catches errors during the execution of
-// following loaders
-// Feel free to use your own if you want to manage other error codes
-export const createErrorBoundaryLoader: () => Loader = () => {
-  return async (ctx, page, next) => {
-    let resultPage: Page | void;
-    try {
-      resultPage = await next();
-    } catch (err) {
-      resultPage = createPage();
-      resultPage.statusCode = err instanceof NotFoundError ? 404 : 500;
-      resultPage.settings.message = err.message;
+function flattenStackTrace(err: Error | RendrError, stack: string[] = []) {
+  if (err instanceof RendrError && err.previousError) {
+    flattenStackTrace(err.previousError, stack);
+  }
+
+  stack.push(err.stack);
+
+  return stack;
+}
+
+type Logger = Partial<{
+  log: (message?: any, ...optionalParams: any[]) => void;
+  error: (message?: any, ...optionalParams: any[]) => void;
+  warn: (message?: any, ...optionalParams: any[]) => void;
+  info: (message?: any, ...optionalParams: any[]) => void;
+}>;
+
+// Behaviour is as follow:
+// 1. Always log the error stack
+// 2. If headers were sent, just ends the response
+// 3. The resultPage receives the message of the error in the key message of its settings
+// 4. In non production mode, resultPage also receives the error trace
+// 5. Client side, the error is thrown as normal for browser to catch it
+export function generateErrorHandler(logger: Logger) {
+  return (err: Error | RendrError, ctx: RequestCtx, page: Page): MaybePage => {
+    const fullStack = flattenStackTrace(err);
+    fullStack.forEach(stack => logger.log(stack));
+
+    if (ctx.res && ctx.res.headersSent) {
+      ctx.res.end();
+      return;
+    }
+
+    const resultPage = createPage();
+
+    resultPage.statusCode = err instanceof NotFoundError ? 404 : 500;
+    resultPage.settings.message = err.message;
+
+    if (ctx.isServerSide && process.env.NODE_ENV !== "production") {
+      resultPage.settings.stackTrace = fullStack;
+    }
+
+    if (ctx.isClientSide) {
+      throw err;
     }
 
     return resultPage;
   };
-};
+}
+
+export function createErrorBoundaryLoader(logger: Logger = console): Loader {
+  return async (ctx, page, next) => {
+    try {
+      return await next();
+    } catch (err) {
+      const handler = generateErrorHandler(logger);
+      return handler(err, ctx, page);
+    }
+  };
+}

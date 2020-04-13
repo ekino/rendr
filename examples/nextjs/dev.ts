7f84c04cab9f1223c740bab8514feb7f02df49af
Thomas Rabaix <thomas.rabaix@gmail.com>
2020-04-13 11:45:03 +0200

    fix(examples/nextjs): make sure the local server start with a reverse proxy

---
+++ examples/nextjs/dev.ts
@@ -0,0 +1,38 @@
+import express from "express";
+import next from "next";
+import { createProxyMiddleware } from "http-proxy-middleware";
+
+const app = next({
+  dir: ".", // base directory where everything is, could move to src later
+  dev: true,
+});
+
+const handle = app.getRequestHandler();
+
+app
+  .prepare()
+  .then(() => {
+    let server = express();
+
+    server.use(
+      createProxyMiddleware("/api", {
+        target: "http://localhost:3000/api",
+        pathRewrite: { "^/api": "/" },
+        changeOrigin: true,
+      })
+    );
+
+    // Default catch-all handler to allow Next.js to handle all other routes
+    server.all("*", (req, res) => handle(req, res));
+
+    server.listen(8000, (err) => {
+      if (err) {
+        throw err;
+      }
+      console.log(`> Ready on port https://localhost:8000 `);
+    });
+  })
+  .catch((err) => {
+    console.log("An error occurred, unable to start the server");
+    console.log(err);
+  });

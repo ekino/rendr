47191af3b36cbd959111665376eeaa1fda5fde88
Thomas Rabaix <thomas.rabaix@gmail.com>
2021-07-22 14:10:57 +0000

    feat(contentful): install the Contentful Editor

---
+++ examples/api-contentful/setup/migrations/00-install-app.js
@@ -0,0 +1,114 @@
+const contentful = require("contentful-management");
+
+async function getOrganizationId(client) {
+  const organizations = await client.getOrganizations();
+  if (organizations.total > 1) {
+    // unable to guess the organisation id, please provide a valid value
+    console.log("\n\n");
+
+    organizations.items.forEach((o) => {
+      console.log(
+        "Please define a CONTENTFUL_ORGANIZATION_ID env value, with one of the following values:"
+      );
+      console.log(
+        ` > ${o.name} => ${o.sys.id} - CONTENTFUL_ORGANIZATION_ID=${o.sys.id}`
+      );
+    });
+
+    console.log("\n\n");
+    throw new Error("Unable to find one organisation");
+  }
+
+  return organizations.items[0].sys.id;
+}
+
+module.exports = async function (migration, { spaceId, accessToken }) {
+  const client = contentful.createClient({
+    accessToken: accessToken,
+  });
+
+  const organisationId =
+    process.env.CONTENTFUL_ORGANIZATION_ID || (await getOrganizationId(client));
+
+  const organisation = await client.getOrganization(organisationId);
+  const appDefinitions = await organisation.getAppDefinitions();
+
+  // check if the app is defined
+  let app = appDefinitions.items.find((appDef) => {
+    return appDef.src.substr(0, 20) === "https://cf.ekino.app";
+  });
+
+  if (app) {
+    console.log(
+      "Ekino Contentful editor is already available in your organisation!\n"
+    );
+    console.log(`Current url: ${app.src}\n`);
+    console.log("Please make sure you are using the latest version:");
+    console.log(
+      "Visit to check latest version: https://cf.ekino.app/editor/v/latest/docs\n"
+    );
+    // app already installed nothing to do
+  } else {
+    app = await client.rawRequest({
+      method: "POST",
+      url: `https://api.contentful.com/organizations/${organisationId}/app_definitions`,
+      data: {
+        name: "Ekino Contentful Editor",
+        src: "https://cf.ekino.app/editor/v/0.2.6",
+        locations: [
+          {
+            location: "entry-sidebar",
+          },
+          {
+            location: "app-config",
+          },
+        ],
+      },
+    });
+
+    console.log(
+      "Ekino Contentful editor has been defined in your organisation!\n"
+    );
+    console.log(`Current url: ${app.src}\n`);
+  }
+
+  // check if the app is installed on the different environment
+  const space = await client.getSpace(spaceId);
+  const environments = await space.getEnvironments();
+
+  for (let i in environments.items) {
+    const env = environments.items[i];
+    const installedApps = await client.rawRequest({
+      method: "GET",
+      url: `https://api.contentful.com/spaces/${spaceId}/environments/${env.sys.id}/app_installations`,
+    });
+
+    const installed = installedApps.items.find((item) => {
+      return app.sys.id === item.sys.appDefinition.sys.id;
+    });
+
+    if (installed) {
+      console.log(
+        `> env: ${env.name} - Application is already installed, nothing to do!`
+      );
+      continue;
+    }
+
+    console.log(`> env: ${env.name} - Application not found...`);
+
+    const result = await client.rawRequest({
+      method: "PUT",
+      url: `https://api.contentful.com/spaces/${spaceId}/environments/${env.sys.id}/app_installations/${app.sys.id}`,
+      data: {
+        parameters: {
+          editorStructure:
+            '{"rendr_article":{"general":{"label":"General","sections":[{"fields":["title","slug","type","abstract"]}]},"content":{"label":"Contents","sections":[{"fields":["blocks"]}]},"SEO":{"label":"SEO","sections":[{"fields":["seo_description","seo_keywords"]}]},"publishing":{"label":"Publishing options","sections":[{"fields":["image_list","image_header","published_at","authors","website"]}]}}}',
+        },
+      },
+    });
+
+    console.log(
+      `> env: ${env.name} - Application has been installed with default rendr settings, please adjust!`
+    );
+  }
+};
